caed77191caf3f3a8a6bd3a55b38e45b4584e92d
Cristian Caram <cjcaram@gmail.com>
2019-02-12 18:07:14 -0300

    fix issue to initialize app for first time

--- src/main/java/com/enano/cloudbean/CloudBeanApplication.java
+++ src/main/java/com/enano/cloudbean/CloudBeanApplication.java
@@ -1,16 +1,10 @@
 package com.enano.cloudbean;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
-import com.enano.cloudbean.dtos.CompanyDto;
-import com.enano.cloudbean.dtos.UserDto;
-import com.enano.cloudbean.entities.Role;
-import com.enano.cloudbean.services.UserService;
 
 @SpringBootApplication
 public class CloudBeanApplication {
@@ -19,24 +13,6 @@ public class CloudBeanApplication {
 		SpringApplication.run(CloudBeanApplication.class, args);
 	}
 	
-	@Bean
-  public CommandLineRunner setupDefaultUser(UserService service) {
-	  if (service.getAllUsers().size() == 0) {
-	    
-      return args -> {
-          service.save(new UserDto(
-                  "admin", //userName
-                  new Role("ADMIN", ""),//role 
-                  null, // id
-                  "123", //password
-                  "123",//Active
-                  new CompanyDto() //Company
-          ));
-      };
-	  }
-	  return null;
-  }
-  
   @Bean
   public PasswordEncoder getPasswordEncoder(){
       return new BCryptPasswordEncoder();
